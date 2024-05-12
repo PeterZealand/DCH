be8c168611f3c41766f564a5e01dde936b3ac597
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-05-12 20:20:05 +0200

    Added interface and made EventJson implement it

---
+++ DCH/Interfaces/IEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using DCH.Models;
+namespace DCH.Interfaces
+
+{
+    public interface IEventRepository
+    {
+        Dictionary<int, Event> AllEvents();
+        Dictionary<int, Event> FilterEvents(string crtiteria);
+        void DeleteEvent(int id);
+        void AddEvent(Event Event);
+        void UpdateEvent(Event Event);
+        Event GetEvent(int id);
+    }
+}
--- DCH/Services/EventJson.cs
+++ DCH/Services/EventJson.cs
@@ -1,19 +1,47 @@
 ﻿using DCH.Helpers;
+using DCH.Interfaces;
 using DCH.Models;
 
 namespace DCH.Services
 {
-    public class EventJson
+    public class EventJson : IEventRepository
     {
         string JsonFileName = @"C:\Users\eriki\OneDrive - Zealand\Semester 1\Afleveringer\DCH\DCH\DCH\Data\JsonEvents.json";
+
+        public void AddEvent(Event Event)
+        {
+            throw new NotImplementedException();
+        }
+
         public Dictionary<int, Event> AllEvents()
         {
             return JsonFileReader.ReadJson(JsonFileName);
         }
+
+        public void DeleteEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, Event> FilterEvents(string crtiteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event GetEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Event GetEvents(int id)
         {
             Dictionary<int, Event> events = AllEvents();
             return events[id];
+        }
+
+        public void UpdateEvent(Event Event)
+        {
+            throw new NotImplementedException();
         }
     }
 }
