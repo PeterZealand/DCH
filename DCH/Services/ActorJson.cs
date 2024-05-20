c3d08bdbbfd27b8ba4855a6e1f509b1ea6f60ff2
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-05-20 14:41:45 +0200

    Rectified Jsons File error

--- DCH/Services/ActorJson.cs
+++ DCH/Services/ActorJson.cs
@@ -7,7 +7,7 @@ namespace DCH.Services
 {
     public class ActorJson : IActorRepository
     {
-        string JsonFileName = @"C:\Users\mlber\source\repos\DCH\DCH\Data\JsonActors.json";
+        string JsonFileName = @"C:\Users\eriki\OneDrive - Zealand\Semester 1\Afleveringer\DCH\DCH\DCH\Data\JsonActors.json";
         //C:\Users\eriki\OneDrive - Zealand\Semester 1\Afleveringer\DCH\DCH\DCH\Data\JsonActors.json
 
         private readonly Dictionary<int, Actor> actors = new Dictionary<int, Actor>();
