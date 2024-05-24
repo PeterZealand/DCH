867c892c73c51c5f01c9d9f67b568a6e9cb7e473
Morten Chami Berner <mob001@edu.zealand.dk>
2024-05-24 09:34:38 +0200

    Validation on PhoneNumber

--- DCH/Pages/Actors/CreateActor.cshtml.cs
+++ DCH/Pages/Actors/CreateActor.cshtml.cs
@@ -2,21 +2,57 @@ using DCH.Interfaces;
 using DCH.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace DCH.Pages.Actors
 {
+    //public class CreateActorsModel : PageModel
+    //{
+    //    private IActorRepository catalog;
+    //    [BindProperty]
+    //    public Actor Actor { get; set; }
+
+    //    public CreateActorsModel(IActorRepository cat)
+    //    {
+    //        catalog = cat;
+    //    }
+
+
+    //    public IActionResult OnGet(int id)
+    //    {
+    //        Actor = new Actor();
+    //        return Page();
+    //    }
+
+    //    public IActionResult OnPost()
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return Page();
+    //        }
+    //        if (Actor.Id == 0)
+    //        {
+    //            catalog.AddActor(Actor);
+    //        }
+    //        return RedirectToPage("GetAllActors");
+    //    }
+
+
+    //}
     public class CreateActorsModel : PageModel
     {
         private IActorRepository catalog;
+
         [BindProperty]
         public Actor Actor { get; set; }
+
+        public string Message { get; set; }
 
         public CreateActorsModel(IActorRepository cat)
         {
             catalog = cat;
         }
-
 
         public IActionResult OnGet(int id)
         {
@@ -30,21 +66,39 @@ namespace DCH.Pages.Actors
             {
                 return Page();
             }
+
+            long parsedPhoneNumber;
+            bool isNumber = long.TryParse(Actor.PhoneNumber, out parsedPhoneNumber);
+
+            if (!isNumber)
+            {
+                ModelState.AddModelError("Actor.PhoneNumber", "Indtast venligst et gyldigt telefonnummer.");
+                return Page();
+            }
+
             if (Actor.Id == 0)
             {
                 catalog.AddActor(Actor);
             }
+
+            Message = $"Telefonnummeret er gyldigt: {parsedPhoneNumber}";
+
             return RedirectToPage("GetAllActors");
         }
-
-        private bool ParsePhoneNumber(string phoneNumber)
+        public class NumericOnlyAttribute : ValidationAttribute
         {
-            // Example: Ensure the phone number is exactly 8 digits long and contains only digits
-            return Regex.IsMatch(phoneNumber, @"^\d{8}$");
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (value != null)
+                {
+                    var strValue = value.ToString();
+                    if (!Regex.IsMatch(strValue, @"^\d+$"))
+                    {
+                        return new ValidationResult("Indtast venligst kun tal for telefonnummer.");
+                    }
+                }
+                return ValidationResult.Success;
+            }
         }
-
-        //public void OnGet()
-        //    {
-        //    }
     }
 }
