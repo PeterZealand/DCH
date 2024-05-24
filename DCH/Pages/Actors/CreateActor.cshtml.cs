a97169b91ca6636433a7c9e62786cc5cc6a0d452
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-05-24 13:41:53 +0200

    removed unused code

--- DCH/Pages/Actors/CreateActor.cshtml.cs
+++ DCH/Pages/Actors/CreateActor.cshtml.cs
@@ -7,52 +7,17 @@ using System.Text.RegularExpressions;
 
 namespace DCH.Pages.Actors
 {
-    //public class CreateActorsModel : PageModel
-    //{
-    //    private IActorRepository catalog;
-    //    [BindProperty]
-    //    public Actor Actor { get; set; }
-
-    //    public CreateActorsModel(IActorRepository cat)
-    //    {
-    //        catalog = cat;
-    //    }
-
-
-    //    public IActionResult OnGet(int id)
-    //    {
-    //        Actor = new Actor();
-    //        return Page();
-    //    }
-
-    //    public IActionResult OnPost()
-    //    {
-    //        if (!ModelState.IsValid)
-    //        {
-    //            return Page();
-    //        }
-    //        if (Actor.Id == 0)
-    //        {
-    //            catalog.AddActor(Actor);
-    //        }
-    //        return RedirectToPage("GetAllActors");
-    //    }
-
-
-    //}
     public class CreateActorsModel : PageModel
     {
         private IActorRepository catalog;
-
         [BindProperty]
         public Actor Actor { get; set; }
-
-        public string Message { get; set; }
 
         public CreateActorsModel(IActorRepository cat)
         {
             catalog = cat;
         }
+
 
         public IActionResult OnGet(int id)
         {
@@ -66,39 +31,13 @@ namespace DCH.Pages.Actors
             {
                 return Page();
             }
-
-            long parsedPhoneNumber;
-            bool isNumber = long.TryParse(Actor.PhoneNumber, out parsedPhoneNumber);
-
-            if (!isNumber)
-            {
-                ModelState.AddModelError("Actor.PhoneNumber", "Indtast venligst et gyldigt telefonnummer.");
-                return Page();
-            }
-
             if (Actor.Id == 0)
             {
                 catalog.AddActor(Actor);
             }
-
-            Message = $"Telefonnummeret er gyldigt: {parsedPhoneNumber}";
-
             return RedirectToPage("GetAllActors");
         }
-        public class NumericOnlyAttribute : ValidationAttribute
-        {
-            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-            {
-                if (value != null)
-                {
-                    var strValue = value.ToString();
-                    if (!Regex.IsMatch(strValue, @"^\d+$"))
-                    {
-                        return new ValidationResult("Indtast venligst kun tal for telefonnummer.");
-                    }
-                }
-                return ValidationResult.Success;
-            }
-        }
+
+
     }
 }
