83ce1f3ed358e264566b88761f4cd1be561a02de
Peter Patrick Eriksen <pete05q5@edu.zealand.dk>
2024-05-25 09:44:59 +0200

    Made ekstra Jsonfilepaths

--- DCH/Services/EventJson.cs
+++ DCH/Services/EventJson.cs
@@ -6,8 +6,10 @@ namespace DCH.Services
 {
     public class EventJson : IEventRepository
     {
+        string JsonFileName = @"C:\Users\eriki\OneDrive - Zealand\Semester 1\Afleveringer\DCH\DCH\DCH\Data\JsonEvents.json";
         //string JsonFileName = @"C:\Users\mlber\source\repos\DCH\DCH\Data\JsonEvents.json";
-        string JsonFileName = @"C:\Users\sskli\source\repos\DCH\DCH\Data\JsonEvents.json";
+        //string JsonFileName = @"C:\Users\sskli\source\repos\DCH\DCH\Data\JsonEvents.json";
+
         
         private readonly Dictionary<int, Event> events = new Dictionary<int, Event>();
         private int currentId = 0;
