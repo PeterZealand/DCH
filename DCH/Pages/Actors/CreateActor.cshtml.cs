d10978b138ec20f18ccf10df4f302a41741e831e
Paprika Seidler <pas002@edu.zealand.dk>
2024-05-27 14:28:41 +0200

    Issues with CreateActor and source is now fixed :-)

--- DCH/Pages/Actors/CreateActor.cshtml.cs
+++ DCH/Pages/Actors/CreateActor.cshtml.cs
@@ -26,39 +26,46 @@ namespace DCH.Pages.Actors
         public IActionResult OnGet(int id)
         {
             Actor = new Actor();
+            Source = Source ?? "actor";
             return Page();
         }
 
         public IActionResult OnPost()
         {
+            Source = Source ?? "actor";
+
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+            //if (Actor.Id == 0)
+            //{
+            //    catalog.AddActor(Actor);
+            //}
+            //return RedirectToPage("ActorLogin");
 
-            
             if (CheckIfActorExists(Actor.Email) == true)
             {
                 // email adressen findes i listen og derfor sendes brugeren tilbage for at oprette sig med en anden email adresse
                 //TODO vi skal ha ændret koden så den giver en fejl i stedet for at sende en tilbage til create actor
-                return RedirectToPage("CreateActor");
+                return RedirectToPage("CreateActor", new {source = Source });
             }
-            else 
-            {   
+            else
+            {
                 // email adressen findes ikke på medlemslisten og derfor bliver medlemet oprettet i systemet og sendes videre til login siden.
-                catalog.AddActor(Actor);        
-                return RedirectToPage("ActorLogin");
+                catalog.AddActor(Actor);
+                return RedirectToPage("ActorLogin", new { source = Source });
             }
-    
-            // Medlemmer må ikke tilgå admin siden med alle medlemmer.
-            //return RedirectToPage("GetAllActors");
+
+           // Medlemmer må ikke tilgå admin siden med alle medlemmer.
+           // return RedirectToPage("GetAllActors");
         }
 
-        
+
         private bool CheckIfActorExists(string email)
         {
             var actors = catalog.AllActors();
-            
+
             // tjek om der eksisterer et medlem med email adressen, som brugeren ønsker at oprette sig med.
             return actors.Values.Any(a =>
             a.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
